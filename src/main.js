7f45f1d069c695d59b0e981cc73568056f1dab2b
hanhan <131468867+anquanxishu@users.noreply.github.com.>
2026-08-15 11:47:08 +0800

    feat(favorStore) 数据持久化 可以保存在localstorage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' // 引入插件
 import 'normalize.css'
 import '@/assets/css/index.css'
 
@@ -7,8 +8,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/stores/favorList.js
+++ src/stores/favorList.js
@@ -1,34 +1,45 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
-export const useFavorListStore = defineStore('favorList', () => {
-  const favorites = ref([])
-  const favorList = computed(() => favorites.value)
-  // 检查是否收藏该房屋
-  const isFavor = (id) => {
-    return favorList.value.some((item) => item.data.houseId === id)
-  }
-  // 添加收藏房屋
-  const addFavor = (houseItem) => {
-    favorites.value.push(houseItem)
-  }
-  // 删除收藏房屋
-  const removeFavor = (houseId) => {
-    favorites.value = favorites.value.filter((item) => item.data.houseId !== houseId)
-  }
-  // 切换收藏状态
-  const toggleFavorite = (houseItem) => {
-    const id = houseItem.data.houseId
-    if (isFavor(id)) {
-      removeFavor(id)
-    } else {
-      addFavor(houseItem)
+export const useFavorListStore = defineStore(
+  'favorList',
+  () => {
+    const favorList = ref([])
+    // 检查是否收藏该房屋
+    const isFavor = (id) => {
+      return favorList.value.some((item) => item.data.houseId === id)
     }
-  }
-  return {
-    favorList,
-    isFavor,
-    addFavor,
-    removeFavor,
-    toggleFavorite,
-  }
-})
+    // 添加收藏房屋
+    const addFavor = (houseItem) => {
+      favorList.value.push(houseItem)
+      // localStorage.setItem('favorite-list', JSON.stringify(favorites.value))
+    }
+    // 删除收藏房屋
+    const removeFavor = (houseId) => {
+      favorList.value = favorList.value.filter((item) => item.data.houseId !== houseId)
+    }
+    // 切换收藏状态
+    const toggleFavorite = (houseItem) => {
+      const id = houseItem.data.houseId
+      if (isFavor(id)) {
+        removeFavor(id)
+      } else {
+        addFavor(houseItem)
+      }
+    }
+
+    return {
+      favorList,
+      isFavor,
+      addFavor,
+      removeFavor,
+      toggleFavorite,
+    }
+  },
+  // ---------- 持久化配置（必须放在第三个参数） ----------
+  {
+    persist: {
+      key: 'favorList',
+      storage: localStorage,
+    },
+  },
+)
