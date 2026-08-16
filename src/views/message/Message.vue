5cf4d04a0aeabdfcb16286eb011b591a54d44e5a
hanhan <131468867+anquanxishu@users.noreply.github.com.>
2026-08-16 21:14:11 +0800

    a

--- src/views/message/Message.vue
+++ src/views/message/Message.vue
@@ -1,50 +1,9 @@
 <template>
   <div class="message">
     <h1>正在开发中，敬请期待！</h1>
-    <p ref="countRef">当前计数: {{ count }}</p>
-    <button @click="increment">增加</button>
-    <button @click="decrement">减少</button>
-    <button @click="reset">重置</button>
   </div>
 </template>
 
-<script setup>
-import { ref, version, nextTick, onUpdated } from 'vue'
-const count = ref(0)
-const countRef = ref(null)
-const increment = () => {
-  count.value++
-  console.log('+++')
-  console.log(countRef.value.textContent)
-  console.log(countRef.value)
-  console.log(countRef.value.innerHTML)
-}
-const decrement = async () => {
-  count.value--
-  console.log('---')
-  await nextTick(() => {
-    console.log(countRef.value.textContent)
-    console.log(countRef.value.innerHTML)
-  })
-  console.log('decrement', countRef.value.innerHTML)
-}
-
-const reset = () => {
-  count.value = 0
-  console.log('reset')
-  console.log(countRef.value.textContent)
-  console.log(countRef.value)
-  console.log(countRef.value.innerHTML)
-}
-onUpdated(() => {
-  if (count.value < 10) {
-    setTimeout(() => {
-      count.value++
-    }, 1000)
-  } else {
-    console.log('over heheieie')
-  }
-})
-</script>
+<script setup></script>
 
 <style scoped lang="scss"></style>
